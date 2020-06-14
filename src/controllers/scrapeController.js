9b12d097804e572d065af6f609ace7bb0c7d1459
rmadhwal <rohanmadhwal@yahoo.co.in>
2020-06-14 18:30:11 +0800

    allow scraping with url

--- src/controllers/scrapeController.js
+++ src/controllers/scrapeController.js
@@ -1,6 +1,9 @@
 import {scrapeSingleProduct} from "../services/scrapeService";
+import url from "url";
+import  querystring from "querystring";
 
 export async function scrapeProduct(req, res) {
-    const singleProductInfo = await scrapeSingleProduct(req.body.productId);
+    const param = req.body.productId === undefined ? querystring.parse(url.parse(req.url).query).productId : req.body.productId;
+    const singleProductInfo = await scrapeSingleProduct(param);
     return res.json(singleProductInfo)
 }
